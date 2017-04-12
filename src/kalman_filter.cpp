3d91c6b3ffc89ac9c614650ffd53aa45b2ec5810
Peng Zhang <penggzhang@126.com>
2017-04-12 11:43:31 +0800

    kalman_filter.cpp PHt multiplication efficiency improved

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -36,9 +36,9 @@ void KalmanFilter::Update(const VectorXd &z) {
   VectorXd z_pred = H_ * x_;
   VectorXd y = z - z_pred;
   MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
   MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd Si = S.inverse();
   MatrixXd K = PHt * Si;
     
   // New estimate
@@ -86,9 +86,9 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 
   // Compute Kalman gain
   MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
   MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd Si = S.inverse();
   MatrixXd K = PHt * Si;
     
   // New estimate
